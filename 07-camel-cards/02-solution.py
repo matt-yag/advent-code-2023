d6aab19ddbe5a5ba82fdff4f5ba691082c410f6a
Matt <mattyag@proton.me>
2023-12-06 23:53:03 -0800

    Cleanup

--- 07-camel-cards/02-solution.py
+++ 07-camel-cards/02-solution.py
@@ -2,12 +2,21 @@ FILE_PATH = 'input.txt'
 with open(FILE_PATH, 'r') as file:
     lines = file.readlines()
 
+CARDS_TO_TYPE = {
+    0: lambda cards_map, jokers: 'Z___FIVE_OF_A_KIND',
+    1: lambda cards_map, jokers: 'Z___FIVE_OF_A_KIND',
+    2: lambda cards_map, jokers: 'Y___FOUR_OF_A_KIND' if (4 - jokers) in cards_map.values() else 'X___FULL_HOUSE',
+    3: lambda cards_map, jokers: 'W___THREE_OF_A_KIND' if (3 - jokers) in cards_map.values() else 'V___TWO_PAIRS',
+    4: lambda cards_map, jokers: 'U___ONE_PAIR',
+    5: lambda cards_map, jokers: 'T___HIGH_CARD',
+}
+
 
 class Hand:
 
     def __init__(self, line):
         (cards, bid) = line.split(' ')
-        self.bid = int(bid.strip())
+        self.bid = int(bid)
         self.comparable_string = (cards
                                   .replace('A', 'E')
                                   .replace('K', 'D')
@@ -20,29 +29,15 @@ class Hand:
             cards_map[card] = cards_map.get(card, 0) + 1
 
         jokers_count = 5 - len(cards.replace('J', ''))
-        different_cards_count = len(cards_map.keys())
-        if different_cards_count <= 1:
-            self.type = 'Z___FIVE_OF_A_KIND'
-        elif different_cards_count == 2:
-            if (4 - jokers_count) in cards_map.values():
-                self.type = 'Y___FOUR_OF_A_KIND'
-            else:
-                self.type = 'X___FULL_HOUSE'
-        elif different_cards_count == 3:
-            if (3 - jokers_count) in cards_map.values():
-                self.type = 'W___THREE_OF_A_KIND'
-            else:
-                self.type = 'V___TWO_PAIRS'
-        elif different_cards_count == 4:
-            self.type = 'U___ONE_PAIR'
-        elif different_cards_count == 5:
-            self.type = 'T___HIGH_CARD'
-        else:
-            raise Exception("Something is wrong")
+        self.type = CARDS_TO_TYPE[len(cards_map.keys())](cards_map, jokers_count)
+
+    @staticmethod
+    def compare(hand):
+        return hand.type, hand.comparable_string
 
 
-hands = [Hand(line) for line in lines]
-hands.sort(key=lambda hand: (hand.type, hand.comparable_string))
+hands = [Hand(line.strip()) for line in lines]
+hands.sort(key=Hand.compare)
 sum = 0
 for (index, hand) in enumerate(hands):
     sum += hand.bid * (index + 1)
